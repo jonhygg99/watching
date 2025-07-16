571d6dc0b66eb1caaf88ec8d7a7d999de0d94cf8
Jonathan Gómez <jonhygg99@gmail.com>
2025-07-16 15:08:52 +0200

    Bigger Image in episode show modal

--- lib/watchlist/episode_info_modal/widgets/episode_header.dart
+++ lib/watchlist/episode_info_modal/widgets/episode_header.dart
@@ -5,11 +5,7 @@ class EpisodeHeader extends StatelessWidget {
   final Map<String, dynamic> episode;
   final String? imageUrl;
 
-  const EpisodeHeader({
-    super.key,
-    required this.episode,
-    this.imageUrl,
-  });
+  const EpisodeHeader({super.key, required this.episode, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +17,9 @@ class EpisodeHeader extends StatelessWidget {
             Expanded(
               child: Text(
                 episode['title'] ?? '',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
+                style: Theme.of(
+                  context,
+                ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
               ),
             ),
             Text(
@@ -38,7 +34,7 @@ class EpisodeHeader extends StatelessWidget {
             borderRadius: BorderRadius.circular(12),
             child: CachedNetworkImage(
               imageUrl: imageUrl!,
-              height: 120,
+              height: 200, // Could be 250, but I feel is worst as UX
               width: double.infinity,
               fit: BoxFit.cover,
             ),
