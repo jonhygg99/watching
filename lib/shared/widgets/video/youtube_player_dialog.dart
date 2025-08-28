31625155910677a5902336e4428ca8080f07449f
jonhygg99 <jonhygg99@gmail.com>
2025-08-28 19:10:10 +0200

    fixed videos fullscreen mode

--- lib/shared/widgets/video/youtube_player_dialog.dart
+++ lib/shared/widgets/video/youtube_player_dialog.dart
@@ -28,6 +28,7 @@ class YoutubePlayerDialog extends HookWidget {
           forceHD: true,
           hideControls: false,
           controlsVisibleAtStart: true,
+          useHybridComposition: true,
         ),
       );
     }, [videoId]);
@@ -46,65 +47,28 @@ class YoutubePlayerDialog extends HookWidget {
       };
     }, []);
 
-    return Dialog(
-      insetPadding: EdgeInsets.zero,
-      backgroundColor: Colors.transparent,
-      child: Stack(
-        alignment: Alignment.topRight,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.3,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        final size = MediaQuery.of(context).size;
+        final isLandscape = orientation == Orientation.landscape;
+        
+        return Dialog(
+          insetPadding: EdgeInsets.zero,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            width: isLandscape ? size.width : size.width,
+            height: isLandscape ? size.height : size.height * 0.3,
             child: YoutubePlayer(
               controller: controller,
               showVideoProgressIndicator: true,
-              aspectRatio: 16 / 9,
+              aspectRatio: isLandscape ? size.width / size.height : 16 / 9,
               onEnded: (_) {
                 Navigator.pop(context);
               },
             ),
           ),
-          Positioned(
-            top: 8.0,
-            right: 8.0,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.black54,
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                icon: const Icon(
-                  Icons.fullscreen,
-                  color: Colors.white,
-                  size: 24.0,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => Scaffold(
-                            backgroundColor: Colors.black,
-                            body: Center(
-                              child: YoutubePlayer(
-                                controller: controller,
-                                showVideoProgressIndicator: true,
-                                aspectRatio: 16 / 9,
-                                onEnded: (_) {
-                                  Navigator.pop(context);
-                                },
-                              ),
-                            ),
-                          ),
-                      fullscreenDialog: true,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
